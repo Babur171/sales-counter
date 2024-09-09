49c174d3bfaae8c24c3a5624fc3b4233f0c57833
babar <baburali894@gmail.com>
2024-09-09 20:09:58 +0500

    Fixed new issue

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,9 +1,9 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 import config from '../config/config';
-import logger from '../config/logger';
+// import logger from '../config/logger';
 
 // const transport = nodemailer.createTransport(config.email.smtp);
-/* istanbul ignore next */
+
 if (config.env !== 'test') {
   // transport
   //   .verify()
@@ -22,7 +22,6 @@ const sendEmail = async (to: string, subject: string, text: string) => {
 
 const sendResetPasswordEmail = async (to: string, token: string) => {
   const subject = 'Reset password';
-  // replace this url with the link to the reset password page of your front-end app
   const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
   const text = `Dear user,
 To reset your password, click on this link: ${resetPasswordUrl}
@@ -32,7 +31,6 @@ If you did not request any password resets, then ignore this email.`;
 
 const sendNewPasswordEmail = async (email: string, password: string) => {
   const subject = 'Created New Employ';
-  // replace this url with the link to the reset password page of your front-end app
   const text = `Dear user,
 Your Email: ${email}
 Your Password: ${password}
@@ -42,7 +40,6 @@ You can use these credentials for login.`;
 
 const sendVerificationEmail = async (to: string, token: string) => {
   const subject = 'Email Verification';
-  // replace this url with the link to the email verification page of your front-end app
   const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
   const text = `Dear user,
 To verify your email, click on this link: ${verificationEmailUrl}`;
