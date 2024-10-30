5ed2adfcb8d8208d197542e8c52a82d68dc7e0a7
Babur <baburali894@gmail.com>
2024-10-31 00:07:13 +0500

    fixed

--- prisma/migrations/20241030190641_init/migration.sql
+++ prisma/migrations/20241030190641_init/migration.sql
@@ -79,7 +79,6 @@ CREATE TABLE "SoldProduct" (
     "productId" INTEGER NOT NULL,
     "quantity" INTEGER NOT NULL,
     "totalPrice" DOUBLE PRECISION NOT NULL,
-<<<<<<<< HEAD:prisma/migrations/20241030190244_init/migration.sql
     "userId" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -106,11 +105,6 @@ CREATE TABLE "Expense" (
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Expense_pkey" PRIMARY KEY ("id")
-========
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "SoldProduct_pkey" PRIMARY KEY ("id")
->>>>>>>> a3a2276174d7fa2b2e48c6c275153eb582993ceb:prisma/migrations/20240917113124_list_data/migration.sql
 );
 
 -- CreateIndex
@@ -136,12 +130,9 @@ ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("cat
 
 -- AddForeignKey
 ALTER TABLE "SoldProduct" ADD CONSTRAINT "SoldProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-<<<<<<<< HEAD:prisma/migrations/20241030190244_init/migration.sql
 
 -- AddForeignKey
 ALTER TABLE "SoldProduct" ADD CONSTRAINT "SoldProduct_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "NewStock" ADD CONSTRAINT "NewStock_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-========
->>>>>>>> a3a2276174d7fa2b2e48c6c275153eb582993ceb:prisma/migrations/20240917113124_list_data/migration.sql
